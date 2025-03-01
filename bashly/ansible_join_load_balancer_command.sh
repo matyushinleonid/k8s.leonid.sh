48ec4213ad622b7c551e03a4e68fcd4ef7a9ceb5
Leonid Matiushin <lmatyushin48@nebius.com>
2025-03-01 17:48:43 +0100

    lb is not a k8s node anymore

--- bashly/ansible_join_load_balancer_command.sh
+++ bashly/ansible_join_load_balancer_command.sh
@@ -2,22 +2,16 @@ inspect_args
 
 public_ipv6="${args['--public-ipv6']}"
 internal_ipv4="${args['--internal-ipv4']}"
-hostname="${args['--hostname']}"
-existing_ctrl_public_ipv6="${args['--existing-ctrl-public-ipv6']}"
 worker_internal_ipv4="${args['--worker-internal-ipv4']}"
 
 echo "Debugging Arguments:"
 echo "Public IPv6: $public_ipv6"
 echo "Internal IPv4: $internal_ipv4"
-echo "Hostname: $hostname"
-echo "Existing Control Plane IPv6: $existing_ctrl_public_ipv6"
 echo "Worker IP(s) for HAProxy: $worker_internal_ipv4"
 
 ansible-playbook playbooks/join_load_balancer.yaml \
   --private-key "~/.ssh/k8s.leonid.sh" \
   -e "public_ipv6=$public_ipv6 \
       internal_ipv4=$internal_ipv4 \
-      hostname=$hostname \
-      existing_ctrl_public_ipv6=$existing_ctrl_public_ipv6 \
       worker_internal_ipv4='$worker_internal_ipv4' \
       ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"

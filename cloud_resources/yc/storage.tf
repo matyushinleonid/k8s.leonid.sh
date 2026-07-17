bfa9b5b64551a4eaffa927ce19f3cdf3a748465e
matyushinleonid <matyushinleonid@users.noreply.github.com>
2026-07-17 10:54:24 +0300

    Minecraft increase disk size (#85)

--- cloud_resources/yc/storage.tf
+++ cloud_resources/yc/storage.tf
@@ -3,7 +3,7 @@ resource "yandex_compute_disk" "minecraft" {
   zone      = var.zone
   name      = "${var.base_name}-minecraft-data"
   type      = "network-ssd"
-  size      = 10
+  size      = 20
 
   labels = {
     managed-by = "terraform"

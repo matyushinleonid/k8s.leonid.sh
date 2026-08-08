5496e7c6099168d3afbf4cb68e10e5c5aa27fefc
Leonid Matyushin <matyushinleonid@gmail.com>
2026-08-08 16:24:12 +0300

    Add dev VM

--- cloud_resources/hetzner/outputs.tf
+++ cloud_resources/hetzner/outputs.tf
@@ -1,15 +1,19 @@
 output "server_id" {
-  value = hcloud_server.amnezia.id
+  value = hcloud_server.dev.id
 }
 
 output "server_ipv4" {
-  value = hcloud_server.amnezia.ipv4_address
+  value = hcloud_server.dev.ipv4_address
 }
 
 output "server_ipv6" {
-  value = hcloud_server.amnezia.ipv6_address
+  value = hcloud_server.dev.ipv6_address
 }
 
 output "server_ipv6_network" {
-  value = hcloud_primary_ip.ipv6.ip_address
+  value = hcloud_primary_ip.dev_ipv6.ip_address
+}
+
+output "socks5_port" {
+  value = var.socks5_port
 }

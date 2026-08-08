2bd4d870ab4183b4280eef2b20677cd9fc334557
matyushinleonid <matyushinleonid@users.noreply.github.com>
2026-08-08 16:26:18 +0300

    Add dev VM (#99)

--- cloud_resources/hetzner/main.tf
+++ cloud_resources/hetzner/main.tf
@@ -1,48 +1,71 @@
-resource "hcloud_ssh_key" "amnezia" {
+locals {
+  labels = {
+    environment = "development"
+    role        = "dev-vm"
+  }
+}
+
+resource "hcloud_ssh_key" "dev" {
   name       = var.name
   public_key = file(pathexpand(var.ssh_public_key_path))
 }
 
-resource "hcloud_primary_ip" "ipv4" {
+resource "hcloud_primary_ip" "dev_ipv4" {
   name        = "${var.name}-ipv4"
   location    = var.location
   type        = "ipv4"
   auto_delete = false
 
-  labels = {
-    service = "amnezia"
-  }
+  labels = local.labels
 }
 
-resource "hcloud_primary_ip" "ipv6" {
+resource "hcloud_primary_ip" "dev_ipv6" {
   name        = "${var.name}-ipv6"
   location    = var.location
   type        = "ipv6"
   auto_delete = false
 
-  labels = {
-    service = "amnezia"
+  labels = local.labels
+}
+
+resource "hcloud_firewall" "dev" {
+  name   = "${var.name}-firewall"
+  labels = local.labels
+
+  rule {
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "22"
+    source_ips  = ["0.0.0.0/0", "::/0"]
+    description = "SSH"
+  }
+
+  rule {
+    direction   = "in"
+    protocol    = "tcp"
+    port        = tostring(var.socks5_port)
+    source_ips  = var.socks5_source_ips
+    description = "Authenticated SOCKS5"
   }
 }
 
-resource "hcloud_server" "amnezia" {
-  name        = var.name
-  location    = var.location
-  server_type = var.server_type
-  image       = var.image
-  ssh_keys    = [hcloud_ssh_key.amnezia.id]
-  backups     = var.backups
+resource "hcloud_server" "dev" {
+  name         = var.name
+  location     = var.location
+  server_type  = var.server_type
+  image        = var.image
+  ssh_keys     = [hcloud_ssh_key.dev.id]
+  firewall_ids = [hcloud_firewall.dev.id]
+  backups      = var.backups
 
   public_net {
     ipv4_enabled = true
-    ipv4         = hcloud_primary_ip.ipv4.id
+    ipv4         = hcloud_primary_ip.dev_ipv4.id
     ipv6_enabled = true
-    ipv6         = hcloud_primary_ip.ipv6.id
+    ipv6         = hcloud_primary_ip.dev_ipv6.id
   }
 
-  labels = {
-    service = "amnezia"
-  }
+  labels = local.labels
 
   shutdown_before_deletion = true
 }
--- cloud_resources/hetzner/variables.tf
+++ cloud_resources/hetzner/variables.tf
@@ -1,6 +1,6 @@
 variable "name" {
   type    = string
-  default = "amnezia-vpn"
+  default = "dev-vm"
 }
 
 variable "location" {
@@ -10,7 +10,7 @@ variable "location" {
 
 variable "server_type" {
   type    = string
-  default = "cx23"
+  default = "cx33"
 }
 
 variable "image" {
@@ -20,10 +20,25 @@ variable "image" {
 
 variable "ssh_public_key_path" {
   type    = string
-  default = "~/.ssh/hetzner-amnezia.pub"
+  default = "~/.ssh/hetzner-dev.pub"
 }
 
 variable "backups" {
   type    = bool
   default = false
+}
+
+variable "socks5_port" {
+  type    = number
+  default = 32768
+
+  validation {
+    condition     = var.socks5_port >= 1024 && var.socks5_port <= 65535
+    error_message = "socks5_port must be between 1024 and 65535."
+  }
+}
+
+variable "socks5_source_ips" {
+  type    = set(string)
+  default = ["0.0.0.0/0", "::/0"]
 }
